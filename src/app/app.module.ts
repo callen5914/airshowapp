7ef076629dc253a3e2684ca40b8bfa5308f4770d
Coach Allen <clayton.allen.us@gmail.com>
2018-02-24 15:56:08 -0600

    Added basic scafolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CardboxComponent } from './cardbox/cardbox.component';
+import { BlogComponent } from './blog/blog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    JumbotronComponent,
+    CardboxComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule
