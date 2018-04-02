ca178f2336f413277a15bc9fdc74d9c860737c07
Aaron Walls <dynathy@gmail.com>
2018-04-02 17:14:32 -0500

    Links in FAQ not working but everything else is kosher

--- src/app/faq/faq.component.ts
+++ src/app/faq/faq.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { RouterLinkActive } from '@angular/router';
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
